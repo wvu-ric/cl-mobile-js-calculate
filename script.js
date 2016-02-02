dba79739cbd7c27b7a4621abcc76b22ffd9914ba
calexan4 <calexan4@mix.wvu.edu>
2016-02-02 13:19:27 -0800

    changes

--- script.js
+++ script.js
@@ -1,34 +1,11 @@
-// Create a function for each of the 4 basic math operations
-
-// Parameters: Number1, Number2
-// Returns: Result
-
-var snum1 = prompt("Enter the first number");
-var snum2 = prompt("Enter the second number");
-
-var num1 = Number(snum1);
-var num2 = Number(snum2);
-
-var op = prompt("Choose an operation? (add, subtract, multiply, or divide)");
-
-var result = 0;
-
-// Call the appropriate function to correctly set the value of result
-
-if(op == "add"){
-	
-
-}else if(op == "subtract"){
-	
-
-}else if(op == "multiply"){
-	
-
-}else if(op == "divide"){
-	
-
+function myFunction() {
+	var "a" = 5
+	var "b" = 10
+	var "c" = 20
+	var "d" = "a + b + c";
+	alert("the value of d is: " + "d");
 }
+myFunction();
 
-// Display result
 
 alert(result);
